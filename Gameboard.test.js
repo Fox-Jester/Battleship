3feb99f40614521564dfa4d5dce3aa04ab5b01e6
William Marshall <willmarshall2424@gmail.com>
2025-08-04 12:11:36 -0400

    test(Gameboard): Add receiveAttack on hit test

--- Gameboard.test.js
+++ Gameboard.test.js
@@ -6,9 +6,16 @@ describe("Gameboard", () => {
         GB.placeShip([1, 1], [1, 2], [1, 3]);
         expect(GB.board[1, 1]).not.toBeNull()
     })
-    it("should be able to receive attacks", () => {
-        const GB = new Gameboard;
-        expect(GB.receiveAttack(1, 1)).toBe("miss");
+    describe("receiveAttack", () => {
+        it("should return miss if it doesn't hit a ship", () => {
+            const GB = new Gameboard;
+            expect(GB.receiveAttack(1, 1)).toBe("miss");
+        })
+        it("should return hit if it hits a ship", () => {
+            const GB = new Gameboard;
+            GB.placeShip([1, 1], [1, 2], [1, 3]);
+            expect(GB.receiveAttack(1, 1)).toBe("hit");
+        })
 
     })
 })
