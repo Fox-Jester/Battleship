9fe7a154d4a1fc02349b4a0f956a8f3f8a56a5d7
William Marshall <willmarshall2424@gmail.com>
2025-08-04 09:55:01 -0400

    test: Add Ship isSunk test

--- Ship.test.js
+++ Ship.test.js
@@ -4,7 +4,11 @@ describe("Ship", () => {
     const BS = new Ship(2);
     it("should have a hit method", () => {
         expect(BS.hit()).toBe("hit")
+    });
+    it("should have a isSunk method", () => {
+        expect(BS.isSunk()).toBeFalsy();
     })
+
     
 
 })
