91d50a61ef6503ee098b7395befa6e714a9e3442
William Marshall <willmarshall2424@gmail.com>
2025-08-05 10:37:05 -0400

    feat(domManager): Add coordinates to createBoard method

--- domManager.js
+++ domManager.js
@@ -7,15 +7,29 @@ const domManager = {
 
     createBoard(){
         const rowArray = []
+        const coordinateArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 
-        for(let i = 0; i <= 9; i++){
+        for(let i = 0; i <= 10; i++){
             const row = document.createElement("div");
+         
+           
             row.classList.add("row");
+            
             const x = i
             
 
-            for(let i = 0; i <= 9; i++){
+            for(let i = 0; i <= 10; i++){
                 const column = document.createElement("div");
+                if(x === 0){
+                    column.classList.add("coordinate");
+                    if(i !== 0){
+                        column.textContent = coordinateArray[(i - 1)];
+                    }
+                }
+                if(i === 0 && x > 0){
+                    column.classList.add("coordinate");
+                    column.textContent = x
+                }
                 column.classList.add("column");
                 column.dataset.coordinate = [x, i];
                 row.appendChild(column);
