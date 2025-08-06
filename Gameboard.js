dcb6aa5f8014bdbc39233318658dcf212837b3a2
William Marshall <willmarshall2424@gmail.com>
2025-08-06 16:51:41 -0400

    feat(Gameboard): Add some methods

--- Gameboard.js
+++ Gameboard.js
@@ -1,4 +1,8 @@
 import Ship from "./Ship.js"
+import boardEvents from "./boardEvents.js";
+import renderBoard from "./renderBoard.js";
+import game from "./index.js";
+
 class Gameboard{
 
     #createBoard(){
@@ -10,28 +14,52 @@ class Gameboard{
         return board;
     }
 
-    constructor(){
+    constructor(playerNum){
+     
+        this.playerNum = playerNum
         this.board = this.#createBoard();
         this.misses = [];
+        this.ships = []
     }
 
     placeShip(...coordinates){
         const ship = new Ship(coordinates.length)
+        this.ships.push(ship);
         coordinates.forEach((coordinate) => {
             this.board[coordinate[0]][coordinate[1]] = ship;
         })
      
     }
 
-    receiveAttack(x, y){
-        if(this.board[x][y] !== null){
-            return (this.board[x][y].hit());
+    receiveAttack([x, y]){
+        if(this.board[x][y] !== null && typeof this.board[x][y] === "object"){
+            this.board[x][y].hit();
+            this.board[x][y] = "hit";
+            this.checkLoss()
         }
         else{
-            this.misses.push([x, y]);
-            return "miss"
+            this.board[x][y] = "miss"
+        }
+        this.resetBoard()
+    }
+
+    resetBoard(){
+        renderBoard(this.board, this.playerNum);
+        boardEvents(this)
+    }
+
+    checkLoss(){
+        const unSunkShips = []
+        this.ships.forEach((ship) => {
+            if(!(ship.isSunk())){
+                unSunkShips.push(ship)
+            }
+        })
+        if(unSunkShips.length === 0){
+            game.onLose();
         }
     }
+    
 }
 
 
