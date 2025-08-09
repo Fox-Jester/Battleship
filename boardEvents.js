328d66c8ec84091ae8bea639925f9c4a2e4fa350
William Marshall <willmarshall2424@gmail.com>
2025-08-09 09:37:29 -0400

    fix(boardEvents): Remove boardEvents file

--- boardEvents.js
+++ boardEvents.js
@@ -1,12 +1,1 @@
 
-
-export default function boardEvents(Gameboard){
-    const board = document.querySelector(`#player${Gameboard.playerNum}-board`);
-
-    const columns = board.querySelectorAll(".column, .ship");
-    columns.forEach(column => column.addEventListener(("click"), () => {
-        const stingData = column.dataset.coordinate
-        const numArray = stingData.split(",").map(string => Number(string));
-        Gameboard.receiveAttack(numArray)
-    }))
-}
