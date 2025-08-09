93a82dae3e29a2743490d26b3bd08e1c195f34c2
William Marshall <willmarshall2424@gmail.com>
2025-08-09 09:44:39 -0400

    feat(PLayer): Add player num as a property of the Player class

--- Player.js
+++ Player.js
@@ -4,7 +4,7 @@ import Gameboard from "./Gameboard.js"
 
 class Player{
     constructor(type, num){
-
+        this.num = num
         this.type = type
         this.gameboard = new Gameboard(num)
     }
